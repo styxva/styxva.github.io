6e817acb355adaec93896cbf27b26a069cc2c372
styx <styxva@icloud.com>
2024-06-09 23:37:40 +0200

    set up next.js

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Website for styx.";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        shell = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_20
+            nodePackages.typescript-language-server
+          ];
+        };
+      in
+      {
+        devShells = {
+          default = shell;
+        };
+      });
+}
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import styles from "./index.module.css";
+import commonStyles from "@/styles/common.module.css"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>styx</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <main>
+        <div className={styles.pfp}>
+          <div className={styles.ring}>
+            <div className={styles.inner}>
+              <img src="/pfp.png" alt="Profile Picture" />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
