78f469cc4aad3a63986964ea2a9cf108e411a214
styxva <styxva@icloud.com>
2024-06-20 19:01:10 +0200

    tos and ppl

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -16,9 +16,44 @@ export default function Home() {
 
         <h1>Privacy Policy</h1>
 
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        <h2>Github Pages Service</h2>
 
-        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+        <p>This Website is hosted as a GitHub Pages website. GitHub may collect User Personal Information from visitors to this GitHub Pages website, including logs of visitor IP addresses, to comply with legal obligations, and to maintain the security and integrity of this Website and the Service. See the GitHub Privacy Statement for details.</p>
+        
+        <h2>Data Collection</h2>
+
+        <h3>Website Analytics</h3>
+
+        <p>This website does not use any analytics tools.</p>
+
+        <h3>Cookie Usage</h3>
+
+        <p>This website does not use any cookies</p>
+
+        <h2>Linked Services</h2>
+
+        <p>This website contains links to other services. If you follow these links, you should become aware of their terms of service.</p>
+
+        <p>The following are links to the respective terms of service.</p>
+
+        <ul>
+          <li>
+            YouTube (Google): <a href="https://www.youtube.com/static?template=terms" target="_blank">TOS</a>, <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a>
+          </li>
+          <li>
+            Instagram (Meta): <a href="https://help.instagram.com/581066165581870" target="_blank">TOS</a>, <a href="https://privacycenter.instagram.com/policy" target="_blank">Privacy Policy</a>
+          </li>
+          <li>
+            Threads (Meta): <a href="https://help.instagram.com/769983657850450" target="_blank">TOS</a>, <a href="https://privacycenter.instagram.com/policy" target="_blank">Privacy Policy</a>
+          </li>
+          <li>
+            TikTok: <a href="https://www.tiktok.com/legal/page/eea/terms-of-service/en" target="_blank">TOS</a>, <a href="https://www.tiktok.com/legal/page/eea/privacy-policy/en" target="_blank">Privacy Policy</a>
+          </li>
+          <li>
+            Twitter (Papa Elon): <a href="https://x.com/en/tos" target="_blank">TOS</a>, <a href="https://x.com/en/privacy" target="_blank">Privacy Policy</a>
+          </li>
+        </ul>
+
       </Section>
 
       <Footer />
