7a8d6fdadec8332c7770cdb142a080d7d67632cb
styx <styxva@icloud.com>
2024-07-02 17:00:01 +0200

    i am a js wizard
    bow before me

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -1,53 +1,32 @@
 import Head from "next/head"
+import { GetStaticPropsResult } from "next"
 import Section from "@/components/section"
 import Footer from "@/components/footer"
 import HomeLink from "@/components/home-link"
 import VideoContainer from "@/components/video-download-container"
+import fs from "fs"
+import path from "path"
 
-import raw from "/video_attrs.tsx"
+interface AudioData {
+  title: string
+  author: string
+  musicTitle: string
+}
 
-import type { GetServerSideProps } from "next"
+interface Props {
+  audioData: AudioData[]
+}
 
-import { read_file_and_process } from "/read_files.tsx"
-import type { AudioData } from "@public/read_files"
-
-// type Props = {
-//   audio_data: AudioData[]
-// }
-//
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   return {
-//     props: {
-//       audio_data: read_file_and_process("/video_attrs")
-//     },
-//   }
-// }
-//
-export default function NotFound() {
-
-  interface AudioData {
-    title: string
-    author: string
-    music_title: string
-  }
-
-  const lines = raw.split('\n')
-
-  const params: AudioData[] = lines.map(line => {
-    const [title, author, music_title] = line.split('|').map(item => item.trim())
-    return { title, author, music_title }
-  })
-
-  const video_array = []
-
-  for (let i = 0; i < params.length; i++) {
-     video_array.push(
+export default function Videos({ audioData }: Props) {
+  let videos = [];
+  for (let i = 0; i < audioData.length; i++) {
+     videos.push(
       <VideoContainer
         key={i}
         href={"#"}
-        title={params[i].title}
-        music={params[i].music_title}
-        author={params[i].author}
+        title={audioData[i].title}
+        music={audioData[i].musicTitle}
+        author={audioData[i].author}
       />
     )
   }
@@ -65,14 +44,12 @@ export default function NotFound() {
 
         <p>On this page, you will find the downloads for all videos of the last month. You can either pick and choose videos, or you can download them all at once. When you download a video, you will get a .zip file, which contains all audio files, raw and edited, in wav and mp3 format, as well as a text file containing the script in Japanese, English and Romaji (phonetics for the Japanese).</p>
         <p>Due to licensing, I can obviously not include the background music in the download. For that reason, the name of the music I used is displayed below the images of each download, and also included in the text file. If you want to add the music to the audio, you will have to do that manually. Please understand.</p>
-        
+
         <h2>Combined Download</h2>
 
         <div id="video-container-container">
 
-          {
-            video_array
-          }
+          {videos}
 
         </div>
 
@@ -83,3 +60,12 @@ export default function NotFound() {
   )
 }
 
+export async function getStaticProps(): GetStaticPropsResult<Props> {
+  const p = path.join(process.cwd(), "data/videos.json")
+  const data = fs.readFileSync(p, "utf8")
+  return {
+    props: {
+      audioData: JSON.parse(data),
+    },
+  }
+}
