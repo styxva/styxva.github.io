2eea9f95f9b1633064d086e2cfd55a1b993ae1b7
styxva <styxva@icloud.com>
2024-08-28 22:55:51 +0200

    temporarily taking commissions offline

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           <div className={styles.intro}>
             <h1>Hi, I'm styx!</h1>
 
-            <p>I am a 22 y.o. voice artist from Celestia. I mainly do voice overs (VOs) in Japanese on my Instagram page, and stream live VO sessions and other fun stuff on my YouTube channel on Saturdays. On this page, you will find all the information regarding commissions you need!</p>
+            <p>I am a 22 y.o. voice artist from Celestia. I mainly do voice overs (VOs) in Japanese on my Instagram page, and stream live VO sessions and other fun stuff on my YouTube channel on Saturdays.</p>
 
             <div className={styles.cta}>
               <ul className={styles.social}>
@@ -34,27 +34,9 @@ export default function Home() {
 
               <ul className={styles.buttons}>
                 <Button href="https://ko-fi.com/styxva" newTab><Heart /> Donate</Button>
-                <Button href="#commissions"><ArrowDown /> Commissions</Button>
               </ul>
             </div>
           </div>
-        </div>
-      </Section>
-
-      <Section id="commissions" isCentered>
-        <h1>Open for Commissions</h1>
-
-        <p>If you need an English, Japanese or German voice over, a short recorded message, or similar; I would be glad to help out! My specialty is monologues, but I am open to anything! You can find examples of my work on my Instagram page, and YouTube channel.</p>
-
-        <p>If you decide to commission me, or send a request; please use the following "Contact" button. It will open your local mail client with a pre-written form, which you can fill out. In case you don't have one installed, or the button doesn't work for you, you can also directly copy the address by clicking the button on the right.</p>
-
-        <div className={styles.contact}>
-          <div className={styles.buttons}>
-            <Button href="mailto:commissions@styx-va.com?subject=Commission&body=Your%20Script:%0D%0A[Insert%20your%20script%20here.%20I%20only%20accept%20Japanese/English/German]%0D%0A%0D%0ADo%20you%20need%20a%20translation%3F%20Yes/No%0D%0A%0D%0AIf%20you%20do%20need%20a%20translation%3B%20choose%20a%20language%20I%20should%20translate%20it%20to%3A%0D%0AGerman/English/Japanese%0D%0A%0D%0ADo%20you%20need%20a%20video%3F%20Yes/No%0D%0A%0D%0ADo%20you%20have%20background%20music%20you%20want%20me%20to%20use%3F%20Yes/No/NA%0D%0A[If%20you%20do,%20please%20add%20it%20as%20an%20attachment]%0D%0A%0D%0ADo%20you%20have%20a%20background%20image%20you%20want%20me%20to%20use%3F%20Yes/No/NA%0D%0A[If%20you%20do,%20please%20add%20it%20as%20an%20attachment]"><Envelope />Contact</Button>
-            <CopyableText content="commissions@styx-va.com" />
-          </div>
-
-          <span>By sending an email to the above address you acknowledge and agree to my <a href="/conditions" target="_blank">Conditions</a> and <a href="/pricing" target="_blank">Pricing</a>.</span>
         </div>
       </Section>
 
